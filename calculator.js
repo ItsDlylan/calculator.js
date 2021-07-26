ddfb5cb68890f7b190c12b6cbff8cfaf34991c79
Dylan Schwindt <dfieldmark69@gmail.com>
2021-07-26 15:28:02 -0700

    Added more Advanced functions for the calculator

--- calculator.js
+++ calculator.js
@@ -8,8 +8,9 @@ const rl = readline.createInterface({
 
 let calculator = function () {
 	rl.question(
-		`What is the next operation you'd like to use? multiplication, division, square root, addition, subtraction, goodbye \n`,
+		`What is the next operation you'd like to use? \nAddition, Subtraction, Multiplication, Division\nSquare root, Squared, Cubed, Exponential, Moding\n-----EXIT----- \n`,
 		function (answer) {
+			answer = answer.toLowerCase();
 			if (
 				answer == 'multiplication' ||
 				answer == '*' ||
@@ -23,9 +24,10 @@ let calculator = function () {
 							`Insert the Second Number. \n`,
 							function (b) {
 								console.log(
-									`Output: ${a} * ${b} = ${a * b} \n`,
+									`------------------\n\nOutput: ${a} * ${b} = ${
+										a * b
+									}\n\n------------------ \n`,
 								);
-
 								calculator();
 							},
 						);
@@ -44,7 +46,9 @@ let calculator = function () {
 							`Insert the Second Number. \n`,
 							function (b) {
 								console.log(
-									`Output: ${a} / ${b} = ${a / b} \n`,
+									`------------------\n\nOutput: ${a} / ${b} = ${
+										a / b
+									}\n\n------------------ \n`,
 								);
 
 								calculator();
@@ -65,7 +69,9 @@ let calculator = function () {
 							`Insert the Second Number. \n`,
 							function (b) {
 								ans = parseInt(a) + parseInt(b);
-								console.log(`Output: ${a} + ${b} = ${ans} \n`);
+								console.log(
+									`------------------\n\nOutput: ${a} + ${b} = ${ans}\n\n------------------ \n`,
+								);
 
 								calculator();
 							},
@@ -86,7 +92,9 @@ let calculator = function () {
 							`Insert the Second Number. \n`,
 							function (b) {
 								console.log(
-									`Output: ${a} - ${b} = ${a - b} \n`,
+									`------------------\n\nOutput: ${a} - ${b} = ${
+										a - b
+									}\n\n------------------ \n`,
 								);
 
 								calculator();
@@ -94,27 +102,114 @@ let calculator = function () {
 						);
 					},
 				);
-			} else if (answer == 'square root' || answer == 'root') {
+			} else if (answer == 'exponential' || answer == 'exponent') {
+				answer = 'power';
+				rl.question(
+					`Perfect To what ${answer} would you like this number to be raised? \nPlease type the Power. \n`,
+					function (a) {
+						rl.question(
+							`What number would you like to be raised to the power of ${a}\n`,
+							function (b) {
+								let ans = b;
+								for (let i = 0; i < a - 1; i++) {
+									ans *= b;
+								}
+								console.log(
+									`------------------\n\nOutput: ${b}^${a} = ${ans}\n\n------------------ \n`,
+								);
+								calculator();
+							},
+						);
+					},
+				);
+			} else if (
+				answer == 'square' ||
+				answer == 'squared' ||
+				answer == '^'
+			) {
+				answer = 'Squared';
+				rl.question(
+					`Perfect what would you like be ${answer}? \nPlease type the first Number. \n`,
+					function (a) {
+						console.log(
+							`------------------\n\nOutput: ${a}^2 = ${
+								a * a
+							}\n\n------------------ \n`,
+						);
+						calculator();
+					},
+				);
+			} else if (
+				answer == 'cube' ||
+				answer == 'cubed' ||
+				answer == '^3'
+			) {
+				answer = 'Cubed';
+				rl.question(
+					`Perfect what would you like be ${answer}? \nPlease type the first Number. \n`,
+					function (a) {
+						console.log(
+							`------------------\n\nOutput: ${a}^3 = ${
+								a * a * a
+							}\n\n------------------ \n`,
+						);
+						calculator();
+					},
+				);
+			} else if (
+				answer == 'square root' ||
+				answer == 'root' ||
+				answer == '√'
+			) {
 				answer = 'square root';
 				rl.question(
 					`Perfect what would you like to ${answer}? \nPlease type it below. \n`,
 					function (a) {
 						console.log(
-							`Output: The Square root of ${a} = ${Math.sqrt(
+							`------------------\n\nOutput: The Square root of ${a} = ${Math.sqrt(
 								`${a}`,
-							)} \n`,
+							)}\n\n------------------ \n`,
 						);
 
 						calculator();
 					},
 				);
-			} else if (answer == 'bye' || answer == 'goodbye') {
+			} else if (
+				answer == '%' ||
+				answer == 'mod' ||
+				answer == 'remainder' ||
+				answer == 'moding'
+			) {
+				answer = 'Remainder';
+				rl.question(
+					`Perfect! What would you like to find the ${answer} of? \nPlease type the first number now. \n`,
+					function (a) {
+						rl.question(
+							`Insert the Second Number. \n`,
+							function (b) {
+								console.log(
+									`------------------\n\nOutput: ${a} % ${b} = ${
+										a % b
+									}\n\n----------------- \n`,
+								);
+
+								calculator();
+							},
+						);
+					},
+				);
+			} else if (
+				answer == 'bye' ||
+				answer == 'goodbye' ||
+				answer == 'exit' ||
+				answer == 'quit'
+			) {
 				console.log(
-					'Goodbye! Check out this link your local web browser while you now have free time\nhttps://bit.ly/3BKztOc',
+					'\nGoodbye! Check out this link your local web browser while you now have free time\nhttps://bit.ly/3BKztOc\n',
 				);
 			} else {
 				console.log(
-					`Sorry that wasn't one of the options, rebooting... `,
+					`Sorry that wasn't one of the options... rebooting... `,
 				);
 
 				setTimeout(function () {
@@ -137,8 +232,9 @@ let calculator = function () {
 
 let begin = function () {
 	rl.question(
-		`Welcome to the calculator app, What operation would you like to use? multiplication, division, square root, addition, subtraction \n`,
+		`Welcome to the calculator app, What operation would you like to use? \nAddition, Subtraction, Multiplication, Division\nSquare root, Squared, Cubed, Exponential, Moding\n-----EXIT----- \n`,
 		function (answer) {
+			answer = answer.toLowerCase();
 			if (
 				answer == 'multiplication' ||
 				answer == '*' ||
@@ -152,7 +248,9 @@ let begin = function () {
 							`Insert the Second Number. \n`,
 							function (b) {
 								console.log(
-									`Output: ${a} * ${b} = ${a * b} \n`,
+									`------------------\n\nOutput: ${a} * ${b} = ${
+										a * b
+									}\n\n------------------ \n`,
 								);
 
 								calculator();
@@ -173,7 +271,9 @@ let begin = function () {
 							`Insert the Second Number. \n`,
 							function (b) {
 								console.log(
-									`Output: ${a} / ${b} = ${a / b} \n`,
+									`------------------\n\nOutput: ${a} / ${b} = ${
+										a / b
+									}\n\n----------------- \n`,
 								);
 
 								calculator();
@@ -194,7 +294,9 @@ let begin = function () {
 							`Insert the Second Number. \n`,
 							function (b) {
 								ans = parseInt(a) + parseInt(b);
-								console.log(`Output: ${a} + ${b} = ${ans} \n`);
+								console.log(
+									`------------------\n\nOutput: ${a} + ${b} = ${ans}\n\n------------------ \n`,
+								);
 
 								calculator();
 							},
@@ -215,7 +317,9 @@ let begin = function () {
 							`Insert the Second Number. \n`,
 							function (b) {
 								console.log(
-									`Output: ${a} - ${b} = ${a - b} \n`,
+									`------------------\n\nOutput: ${a} - ${b} = ${
+										a - b
+									}\n\n------------------ \n`,
 								);
 
 								calculator();
@@ -223,27 +327,115 @@ let begin = function () {
 						);
 					},
 				);
-			} else if (answer == 'square root' || answer == 'root') {
+			} else if (
+				answer == 'square' ||
+				answer == 'squared' ||
+				answer == '^'
+			) {
+				answer = 'Squared';
+				rl.question(
+					`Perfect what would you like be ${answer}? \nPlease type the first Number. \n`,
+					function (a) {
+						console.log(
+							`------------------\n\nOutput: ${a}^2 = ${
+								a * a
+							}\n\n------------------ \n`,
+						);
+						calculator();
+					},
+				);
+			} else if (
+				answer == 'cube' ||
+				answer == 'cubed' ||
+				answer == '^3'
+			) {
+				answer = 'Cubed';
+				rl.question(
+					`Perfect what would you like be ${answer}? \nPlease type the first Number. \n`,
+					function (a) {
+						console.log(
+							`------------------\n\nOutput: ${a}^3 = ${
+								a * a * a
+							}\n\n------------------ \n`,
+						);
+						calculator();
+					},
+				);
+			} else if (answer == 'exponential' || answer == 'exponent') {
+				answer = 'power';
+				rl.question(
+					`Perfect To what ${answer} would you like this number to be raised? \nPlease type the Power. \n`,
+					function (a) {
+						rl.question(
+							`What number would you like to be raised to the power of ${a}\n`,
+							function (b) {
+								let ans = b;
+								for (let i = 0; i < a - 1; i++) {
+									ans *= b;
+								}
+								console.log(
+									`------------------\n\nOutput: ${b}^${a} = ${ans}\n\n------------------ \n`,
+								);
+								calculator();
+							},
+						);
+					},
+				);
+			} else if (
+				answer == 'square root' ||
+				answer == 'root' ||
+				answer == '√'
+			) {
 				answer = 'square root';
 				rl.question(
 					`Perfect what would you like to ${answer}? \nPlease type it below. \n`,
 					function (a) {
 						console.log(
-							`Output: The Square root of ${a} = ${Math.sqrt(
+							`------------------\n\nOutput: The Square root of ${a} = ${Math.sqrt(
 								`${a}`,
-							)} \n`,
+							)}\n\n------------------ \n`,
 						);
 
 						calculator();
 					},
 				);
-			} else if (answer == 'bye' || answer == 'goodbye') {
-				console.log(
-					'Goodbye! Check out this link your local web browser while you now have free time\nhttps://bit.ly/3BKztOc',
+			} else if (
+				answer == '%' ||
+				answer == 'mod' ||
+				answer == 'remainder' ||
+				answer == 'moding'
+			) {
+				answer = 'Remainder';
+				rl.question(
+					`Perfect! What would you like to find the ${answer} of? \nPlease type the first number now. \n`,
+					function (a) {
+						rl.question(
+							`Insert the Second Number. \n`,
+							function (b) {
+								console.log(
+									`------------------\n\nOutput: ${a} % ${b} = ${
+										a % b
+									}\n\n----------------- \n`,
+								);
+
+								calculator();
+							},
+						);
+					},
 				);
+			} else if (
+				answer == 'bye' ||
+				answer == 'goodbye' ||
+				answer == 'exit' ||
+				answer == 'quit'
+			) {
+				console.log(
+					'\nGoodbye! Check out this link your local web browser while you now have free time\nhttps://bit.ly/3BKztOc\n',
+				);
+				rl.close();
 			} else {
 				console.log(
-					`Sorry that wasn't one of the options, rebooting... `,
+					`Sorry that wasn't one of the options... rebooting... `,
 				);
 
 				setTimeout(function () {
@@ -263,5 +455,4 @@ let begin = function () {
 		},
 	);
 };
-
 begin();
